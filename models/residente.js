1fd2d4dd49884abb799318cb3529e411dc624a03
rodripgg <110933439+rodripgg@users.noreply.github.com>
2022-11-14 00:22:33 -0300

    Create residente.js

---
+++ models/residente.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ResidenteSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    numeroHogar: {
+        type: Number,
+        required: true
+    },
+
+});
+
+module.exports = mongoose.model('Residente', ResidenteSchema);
