f8547539ef79bbbd33643b1b15f5a14076c00d5a
BayronMardones <116137955+BayronMardones@users.noreply.github.com>
2022-11-15 01:26:30 -0300

    corregido

--- models/residente.js
+++ models/residente.js
@@ -12,8 +12,5 @@ const ResidenteSchema = new Schema({
 
 });
 
-<<<<<<< HEAD
 module.exports = mongoose.model('Residente', ResidenteSchema);
-=======
-module.exports = mongoose.model('Residente', ResidenteSchema);
->>>>>>> 1b9e34cc779826b2eb6652e65d39e0554f1920e8
+
