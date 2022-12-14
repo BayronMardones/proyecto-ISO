9bda4906ea960f08599ff05103a52f0389fb77ad
BayronMardones <116137955+BayronMardones@users.noreply.github.com>
2022-12-14 01:06:01 -0300

    estadoRoutes detalle

--- routes/estadoRoutes.js
+++ routes/estadoRoutes.js
@@ -4,6 +4,7 @@ const estadoController = require('../controllers/estadoController');
 
 api.post('/estado', estadoController.createEstado);
 api.get('/estados', estadoController.getEstados);
+api.get('/estado/search/:id', estadoController.getEstado);
 api.delete('/estado/delete/:id', estadoController.deleteEstado);
 api.put('/estado/update/:id', estadoController.updateEstado);
 
