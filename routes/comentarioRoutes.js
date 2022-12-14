47f5a575f421bb34d8574ea874b1cee84be04db7
BayronMardones <116137955+BayronMardones@users.noreply.github.com>
2022-12-14 18:18:16 -0300

    comentario detalle

--- routes/comentarioRoutes.js
+++ routes/comentarioRoutes.js
@@ -4,5 +4,6 @@ const comentarioController = require('../controllers/comentarioController');
 
 api.post('/comentario', comentarioController.createComentario);
 api.get('/comentarios', comentarioController.getComentarios);
-
+api.put('/comentario/update/:id', comentarioController.updateComentario);
+api.delete('/comentario/delete/:id', comentarioController.deleteComentario);
 module.exports = api;
