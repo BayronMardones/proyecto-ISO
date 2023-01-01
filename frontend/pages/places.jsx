dba3b405ebbd52c11e3be265a89ef735507bdb56
rodrigopgg <rodrigoparra08@gmail.com>
2023-01-01 20:23:50 -0300

    Select en espacios

--- frontend/pages/places.jsx
+++ frontend/pages/places.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { Button, Input, Stack, Container, Heading, FormControl, FormLabel, Textarea, Box} from '@chakra-ui/react'
+import { Button, Input, Stack, Container, Heading, FormControl, FormLabel, Textarea, Box, Select} from '@chakra-ui/react'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 import {useRouter, Router} from 'next/router'
@@ -73,12 +73,17 @@ const places = () => {
                         <Input placeholder="ej: 20" type={"number"} onChange={onChange} name={"capacidad"}/>
                     </FormControl>
                     <FormControl>
-                        <FormLabel>Descripcion del espacio</FormLabel>
-                        <Textarea placeholder="Ideal para cumpleaños" type={"text"} onChange={onChange} name={"descripcion"}/>
+                    <FormLabel>Estado del espacio</FormLabel>
+                        <Select placeholder="Selecciona una opción" onChange={onChange} name={"estado"}>
+                            <option value='DISPONIBLE'>Disponible</option>
+                            <option value='RESERVADO'>Reservado</option>
+                            <option value='EN ESPERA'>En espera</option>
+                            <option value='EN MANTENCIÓN'>En Mantención</option>
+                        </Select>
                     </FormControl>
                     <FormControl>
-                        <FormLabel>Estado del espacio</FormLabel>
-                        <Input placeholder="Disponible para su uso" type={"text"} onChange={onChange} name={"estado"}/>
+                        <FormLabel>Descripcion del espacio</FormLabel>
+                        <Textarea placeholder="Ideal para cumpleaños" type={"text"} onChange={onChange} name={"descripcion"}/>
                     </FormControl>
                     <Button colorScheme='teal' variant='outline' type='submit' onClick={onSubmit}>Crear espacio</Button>
                     <Button colorScheme='red' variant='outline' onClick={()=>router.push('/place')}>Cancelar</Button>
