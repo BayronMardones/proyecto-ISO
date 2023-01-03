754ffc6e376d6eb1fc7306428d063f37d3c54a73
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-01-03 07:21:29 -0300

    eliminar

--- frontend/pages/residente/[residenteC].jsx
+++ frontend/pages/residente/[residenteC].jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
+import {Container, HStack, Button, Heading, Stack, Td, Tr, Box, Thead, Table } from '@chakra-ui/react'
 
 export async function getServerSideProps(context){
     try{
@@ -24,13 +25,50 @@ export async function getServerSideProps(context){
 
 const residenteC = (data) => {
 
+   
     const router = useRouter()
-    const {residenteC} = router.query
     const [residente] = useState(data)
     console.log(residente)
 
+    const deleteResidente = (data) =>{
+        try{
+            axios.delete(`${process.env.API_URL}/residente/delete/${residente.data._id}`)
+        router.reload()
+        }catch(err){
+            console.log(err)
+        }
+        
+    }
+
     return (
-        <h1>La pagina es {residente.data.name}</h1>
+        <Container centerContent>
+
+            <Heading>{residente.data.name}</Heading>
+            <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Td>ID</Td>
+                <Td>Hogar</Td>
+                <Td>Rol</Td>
+                <Td>Sancion</Td>
+                </Tr>
+                <Tr>
+                    <Td>{residente.data._id}</Td>
+                    <Td>{residente.data.numeroHogar}</Td>
+                    <Td>{residente.data.rol}</Td>
+                    <Td>{residente.data.sanciones}</Td>
+                </Tr>
+             </Thead>
+                
+            </Table>
+
+            <HStack w={"full"} py={10}>
+                
+                <Button colorScheme='red' variant='outline' onClick={() => deleteResidente(residente.data._id)}>Eliminar</Button>
+                <Button colorScheme='blue' variant='outline' onClick={() => router.push('/residente')}>Cancelar</Button>
+            </HStack>
+
+        </Container>
     )
 }
 
