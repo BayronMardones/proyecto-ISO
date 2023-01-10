4407b487e79635a9743decd62e5f013e8063e25b
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-01-10 16:21:36 -0300

    correccion para servidor 1

--- frontend/pages/estado/[estadoC].jsx
+++ frontend/pages/estado/[estadoC].jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import {Container, HStack, Button, Heading } from '@chakra-ui/react'
-import { getEstado } from '../../data/Estado'
+import { getEstado } from '../../data/estado'
 
 export async function getServerSideProps(context){
     try{
