614ab0ff587e1b4fdb1f665f73ff8e8096555ccb
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-01-10 16:44:15 -0300

    eliminado de espacio

--- frontend/pages/place/ver/[placeC].jsx
+++ frontend/pages/place/ver/[placeC].jsx
@@ -3,10 +3,11 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import {Container, HStack, Button, Heading } from '@chakra-ui/react'
 import { getPlaces } from '../../../data/places'
+import Swal from 'sweetalert2'
 
 export async function getServerSideProps(context){
     try{
-        const response = await getPlaces(context.query.placeC)
+        const response = await axios.get(`${process.env.API_URL}/place/search/${context.params.placeC}`)
 
         // await axios.get(`${process.env.API_URL}/place/search/${context.params.placeC}`)
         return {
@@ -26,23 +27,51 @@ export async function getServerSideProps(context){
 
 
 
-const placeC = ( {data} ) => {
+const placeC = ( data ) => {
 
     const router = useRouter()
-    const {placeC} = router.query
+    // const {placeC} = router.query
     const [place] = useState(data)
     console.log(place)
     
+
+    const deletePlace = (data) =>{
+        try {
+            axios.delete(`${process.env.API_URL}/place/delete/${place.data._id}`)
+            console.log(response.status)
+            router.push('/place')
+            if(response.status === 200){
+                Swal.fire({
+                    title: 'Error',
+                    text: 'no se ha podido eliminar el espacio',
+                    icon: 'error',
+                    confirmButtonText: 'ok'
+                })
+            }
+        } catch (err) {
+            console.log(err)
+            Swal.fire({
+                title: 'espacio Eliminado',
+                text: 'El espacio a sido eliminado de manera exitosa',
+                icon: 'success',
+                confirmButtonText: 'ok'
+            }).then((result)=>{
+                if(result.isConfirmed){
+                    router.push('/place')
+                }
+            })
+        }
+    }
 
     return (
 
         <Container>
 
-            <Heading>{place.name}</Heading>
+            <Heading>{place.data.name}</Heading>
 
             <HStack w={"full"} py={10}>
-                <Button colorScheme='yellow' variant='outline' onClick={() => router.push(`/producto/editar/${product._id}`)}>Editar</Button>
-                <Button colorScheme='red' variant='outline' onClick={() => router.push(`place/delete/${place._id}`)}>Eliminar</Button>
+                {/* <Button colorScheme='yellow' variant='outline' onClick={() => router.push(`/producto/editar/${product._id}`)}>Editar</Button> */}
+                <Button colorScheme='red' variant='outline' onClick={() => deletePlace(place.data._id)}>Eliminar</Button>
                 <Button colorScheme='blue' variant='outline' onClick={() => router.push('/place')}>Cancelar</Button>
             </HStack>
 
